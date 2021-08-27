d9060278d35480c754fbd9057684f085cce714d5
David Danialy <david.danialy@sjsu.edu>
2021-08-26 22:29:20 -0700

    modified script

--- thispersondoesnotexist/thispersondoesnotexist.py
+++ thispersondoesnotexist/thispersondoesnotexist.py
@@ -5,7 +5,7 @@ import cv2
 from utils.opencv_face_detection import cv2_face_cropper
 
 #variable
-image_counts=5 #saving 20k images
+image_counts=20000 #saving 20k images
 
 #newfolder
 newpath = r'./DoesnotExistData' 
@@ -22,9 +22,9 @@ with open(csv_path) as csvfile:
         for row in reader:
             saved.append(row[0])
 
-i = 0
+i = 1
 cropper = cv2_face_cropper()
-while (i < image_counts):
+while (i < image_counts + 1):
     # Using function
     picture = get_online_person()
     checksum2 = get_checksum_from_picture(picture)  # Method is optional, defaults to "md5"
@@ -33,12 +33,11 @@ while (i < image_counts):
         saved.append(checksum2)
         f.write(checksum2+'\n')
 
-        save_picture(picture, './DoesnotExistData/fake/{}.jpg'.format(checksum2))
-        picture = cropper.getfaces('./DoesnotExistData/fake/{}.jpg'.format(checksum2))[0]
-        cv2.imwrite('./DoesnotExistData/fake/{}.jpg'.format(checksum2), picture)
-
+        save_picture(picture, 'C:/SSD_Dataset/Combined_Dataset/Training/Fake/DNE_{}.jpg'.format(i))
+        picture = cropper.getfaces('C:/SSD_Dataset/Combined_Dataset/Training/Fake/DNE_{}.jpg'.format(i))[0]
+        picture = cv2.resize(picture, (256, 256))
+        cv2.imwrite('C:/SSD_Dataset/Combined_Dataset/Training/Fake/DNE_{}.jpg'.format(i), picture)
+        print('{} / {}'.format(i,image_counts))
         i += 1
-    
-    print('{} / {}'.format(i,image_counts))
 
 f.close()
