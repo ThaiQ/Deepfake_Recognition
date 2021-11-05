3c2255fbe1d24b9385f00a4c973947841f80fdb2
law-nguyen <e.lawrence.nguyen@gmail.com>
2021-11-05 16:29:36 -0700

    working version -- and clean

--- server/videosplitter.py
+++ server/videosplitter.py
@@ -5,37 +5,38 @@ import imageio
 from os import listdir
 from os.path import isfile, join
 from glob import glob
+from Deepfake_Detection_NN.utils.predict import predict_visual
 
 # set video file path of input video with name and extension
-path_to_data = './data/Fake_Videos/'
-path_to_labeled_data = './data/Fake_Unlabeled_Images/'
+# path_to_data = './data/Fake_Videos/'
+# path_to_labeled_data = './data/Fake_Unlabeled_Images/'
 
-onlyfiles = [f for f in listdir(path_to_data) if isfile(join(path_to_data, f))] #Get list with all name of files in directory specified
+# onlyfiles = [f for f in listdir(path_to_data) if isfile(join(path_to_data, f))] #Get list with all name of files in directory specified
 
-for value in onlyfiles:
-    vid = cv2.VideoCapture(join(path_to_data, value))
-    name = value[:-4]
-    if not os.path.exists(path_to_labeled_data + name): #Create Path to save images to
-        os.makedirs(path_to_labeled_data + name) 
-    #for frame identity
-    print("Working on video " + name)
-    index = 0
-    imgcount = 0
-    while(True):
-        # Extract images
-        ret, frame = vid.read()
-        # end of frames
-        if not ret: 
-            break
-        # Saves images
-        if (index % 20 == 0):
-            path = path_to_labeled_data + name + '/' + name + '_' + str(imgcount) + '.jpg' #Path to save image to with image name
-            cv2.imwrite(path, frame)
-            print(name + '/' + name + '_' + str(imgcount) + '.jpg')
-            imgcount += 1
+# for value in onlyfiles:
+#     vid = cv2.VideoCapture(join(path_to_data, value))
+#     name = value[:-4]
+#     if not os.path.exists(path_to_labeled_data + name): #Create Path to save images to
+#         os.makedirs(path_to_labeled_data + name) 
+#     #for frame identity
+#     print("Working on video " + name)
+#     index = 0
+#     imgcount = 0
+#     while(True):
+#         # Extract images
+#         ret, frame = vid.read()
+#         # end of frames
+#         if not ret: 
+#             break
+#         # Saves images
+#         if (index % 20 == 0):
+#             path = path_to_labeled_data + name + '/' + name + '_' + str(imgcount) + '.jpg' #Path to save image to with image name
+#             cv2.imwrite(path, frame)
+#             print(name + '/' + name + '_' + str(imgcount) + '.jpg')
+#             imgcount += 1
 
-        # next frame
-        index += 1
+#         # next frame
+#         index += 1
     
 def split_vid(vid_path):
     path = './uploads/video_frames'
@@ -59,8 +60,14 @@ def stitch_vid():
 
   img_siz = (224,224)
   images = []
+  save_path = './uploads/predicted_frames/'
+  if not os.path.exists(save_path):
+      os.makedirs(save_path)
   for image in glob('./uploads/video_frames/*.jpg'):
-      _, prediction = predict_visual(image_resize_value=img_siz, model_paths=models, path_to_img=[image], save=None, draw = True, show = False)
-      images.append(imageio.read(prediction))
-      path = './uploads/'
-      imageio.mimsave(path + 'prediction.gif', images) #name can be chagned later
+      predict_visual(image_resize_value=img_siz, model_paths=models, path_to_img=[image], save=save_path, draw = True, show = False)
+      os.remove(image)
+  for image in glob('./uploads/predicted_frames/*.png'):
+      images.append(imageio.imread(image))
+      os.remove(image)
+  gif_path = './uploads/'
+  imageio.mimsave(gif_path + 'prediction.gif', images) #name can be chagned later
