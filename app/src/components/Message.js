2df10e8ff2c14740a30733e5bcf078a65cb6cf6a
Anh Hoang <anh.t.hoang@sjsu.edu>
2021-11-15 01:56:20 -0800

    add validation and styling

--- app/src/components/Message.js
+++ app/src/components/Message.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 const Message = ({ msg }) => {
   return (
-    <div className='alert alert-info alert-dismissible fade show' role='alert'>
+    <div className='alert' role='alert'>
       {msg}
-      <button
+      {/* <button
         type='button'
         className='close'
         data-dismiss='alert'
         aria-label='Close'
       >
         <span aria-hidden='true'>&times;</span>
-      </button>
+      </button> */}
     </div>
   );
 };
